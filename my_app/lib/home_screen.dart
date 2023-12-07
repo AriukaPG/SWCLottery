5c7c7d264634f3bb116ca0663fd9745170eac88e
AriukaPG <112913238+AriukaPG@users.noreply.github.com>
2023-12-07 14:46:43 +0800

    Update home_screen.dart

--- my_app/lib/home_screen.dart
+++ my_app/lib/home_screen.dart
@@ -1,34 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:my_app/firebase_options.dart';
 import 'package:intl/intl.dart';
+
 late DocumentSnapshot userSnapshot;
 double balance=0;
 double totalIncome=0;
 double totalExpense=0;
 String name = '';
-int _selectedIndex = 0;
 String user='';
 List<TransactionData> transactions = [];
-const List<Widget> _widgetOptions = <Widget>[
-  Text(
-    'Index 0: Home',
-
-  ),
-  Text(
-    'Index 1: Bar Chart',
-
-  ),
-  Text(
-    'Index 2: Wallet',
-
-  ),
-  Text(
-    'Index 3: User',
-
-  ),
-];
 
 FirebaseFirestore db = FirebaseFirestore.instance;
 class HomeScreen extends StatefulWidget {
@@ -37,11 +19,11 @@ class HomeScreen extends StatefulWidget {
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
-
 }
 
 class _HomeScreenState extends State<HomeScreen> {
   late User? _user;
+
   @override
   void initState() {
     super.initState();
@@ -60,42 +42,36 @@ class _HomeScreenState extends State<HomeScreen> {
             .collection('users')
             .doc(_user!.uid)
             .get();
-
         if (userSnapshot.exists) {
             name = userSnapshot['displayName'];
             user = userSnapshot['uid'];
-
             QuerySnapshot<Map<String, dynamic>> transactionsSnapshot =
                 await FirebaseFirestore.instance
                 .collection('transactions')
                 .where('userId', isEqualTo: _user!.uid)
                 .get();
-
             for (QueryDocumentSnapshot<Map<String, dynamic>> transaction
             in transactionsSnapshot.docs) {
               double amount = double.parse(transaction['amount']);
               String type = transaction['type'];
-
               if (type == 'income') {
                 totalIncome += amount;
-                //print(totalIncome);
               } else if (type == 'expense') {
-                totalExpense += amount;
-                //print(totalExpense);
-;              }
-
+                totalExpense += amount;             
+              }
             }
             setState(() {
-              //totalIncome=totalIncome;
               balance=totalIncome-totalExpense;
             });
-          //});
         }
       } catch (error) {
-        print('Error fetching user data: $error');
+        if (kDebugMode) {
+          print('Error fetching user data: $error');
+        }
       }
     }
   }
+
   Future<void> _fetchLastFourTransactions() async {
     if (_user != null) {
       try {
@@ -106,43 +82,15 @@ class _HomeScreenState extends State<HomeScreen> {
             .orderBy('date', descending: true)
             .limit(4)
             .get();
-
         transactions = transactionsSnapshot.docs
             .map((doc) => TransactionData.fromMap(doc.data()))
             .toList();
-
-        //setState(() {});
       } catch (error) {
-        print('Error fetching last four transactions: $error');
+        if (kDebugMode) {
+          print('Error fetching last four transactions: $error');
+        }
       }
     }
-  }
-
-  static const TextStyle optionStyle =
-  TextStyle(fontSize: 35, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Bar Chart',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Wallet',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: User',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
   }
 
   @override
@@ -161,7 +109,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),)),
             Positioned(top: 98,
                 left: 24,
-                child: Text('$name',
+                child: Text(name,
                   style: const TextStyle(
                     color: Colors.white,
                     fontSize: 20
@@ -173,13 +121,12 @@ class _HomeScreenState extends State<HomeScreen> {
                   height: 35,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(5),
-                        color:Color(0xfff69457)
+                        color:const Color(0xfff69457)
                   ),
-                    child: Icon(Icons.notifications_none,
+                    child: const Icon(Icons.notifications_none,
                     color: Colors.white,
                     size: 25,
                   ),
-
                 )),
               Positioned(top: 99,
                   right: 33,
@@ -187,17 +134,16 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
             Column(
               children: [
-                SizedBox(height: 150,),
+                const SizedBox(height: 150,),
                 Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
                   child: Container(
                   width: 400, // Adjust the size as needed
                   height: 190, // Set the same value as the width
                   decoration: BoxDecoration(
-                      color: Color(0xfff69457),
+                      color: const Color(0xfff69457),
                       borderRadius: BorderRadius.circular(20),
                     ),
-                // Change the color as desired
                 ),
                 ),
               ],
@@ -206,7 +152,7 @@ class _HomeScreenState extends State<HomeScreen> {
                top: 170,
                  left: 40,
                  child: Row(
-                   children: [Text("Нийт үлдэгдэл",style: TextStyle(
+                   children: const [Text("Нийт үлдэгдэл",style: TextStyle(
                        fontSize: 18,
                        color: Colors.white
                    ),),
@@ -214,16 +160,16 @@ class _HomeScreenState extends State<HomeScreen> {
                    ],
                  )
           ),
-          Positioned(
+            const Positioned(
                 top: 170,
                 right: 40,
                 child: Icon(Icons.more_horiz, color: Colors.white,),
             ),
-         Positioned(
+          Positioned(
           top:200,
           left: 40,
           child: Text('$balance',
-            style: TextStyle(
+            style: const TextStyle(
                 color: Colors.white,
                 fontSize: 35,
               fontFamily: 'Inter-Regular'
@@ -237,16 +183,16 @@ class _HomeScreenState extends State<HomeScreen> {
                     Container(
                       width: 25,
                       height: 25,
-                      decoration: BoxDecoration(
+                      decoration: const BoxDecoration(
                         shape: BoxShape.circle,
                         color: Color(0xffF58742),
                       ),
-                      child: Center(
+                      child: const Center(
                         child: Icon(Icons.arrow_downward, color:Colors.white, size:15,)
                       ),
                     ),
-                    SizedBox(height: 20,),
-                    Text("  Орлого", style: TextStyle(color: Colors.white, fontSize: 15),)
+                    const SizedBox(height: 20,),
+                    const Text("  Орлого", style: TextStyle(color: Colors.white, fontSize: 15),)
                   ],
                 )
             ),
@@ -254,7 +200,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 top:290,
                 left: 55,
                 child: Text('$totalIncome',
-                  style: TextStyle(
+                  style: const TextStyle(
                       color: Colors.white,
                       fontSize: 22,
                       fontFamily: 'Inter-Regular'
@@ -268,16 +214,16 @@ class _HomeScreenState extends State<HomeScreen> {
                     Container(
                       width: 25,
                       height: 25,
-                      decoration: BoxDecoration(
+                      decoration: const BoxDecoration(
                         shape: BoxShape.circle,
                         color: Color(0xffF58742),
                       ),
-                      child: Center(
+                      child: const Center(
                           child: Icon(Icons.arrow_upward, color:Colors.white, size:15,)
                       ),
                     ),
-                    SizedBox(height: 20,),
-                    Text("  Зарлага", style: TextStyle(color: Colors.white, fontSize: 15),)
+                    const SizedBox(height: 20,),
+                    const Text("  Зарлага", style: TextStyle(color: Colors.white, fontSize: 15),)
                   ],
                 )
             ),
@@ -285,26 +231,24 @@ class _HomeScreenState extends State<HomeScreen> {
                 top:290,
                 right: 55,
                 child: Text('$totalExpense',
-                  style: TextStyle(
+                  style: const TextStyle(
                       color: Colors.white,
                       fontSize: 22,
                       fontFamily: 'Inter-Regular'
                   ),)
             ),
-          ],
-
-          ),
-          SizedBox(height: 30),
+          ],),
+          const SizedBox(height: 30),
           Row(
-            children: [
+            children: const [
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
+                padding: EdgeInsets.symmetric(horizontal: 20),
                 child: Text("Гүйлгээний түүх", style:
                 TextStyle(fontSize: 20,
                     fontWeight: FontWeight.bold),),
               ),
               Padding(
-                padding: const EdgeInsets.fromLTRB(80, 0, 0, 0),
+                padding: EdgeInsets.fromLTRB(80, 0, 0, 0),
                 child: Text("Бүгдийг харах", style:
                 TextStyle(fontSize: 16,
                    ),
@@ -312,101 +256,16 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ],
           ),
-          /*SizedBox(height: 10),
-          ListTile(
-            title: Text("Upwork",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-            subtitle: Text("Өнөөдөр"),
-            leading: Container(
-              width: 50,
-              height: 50,
-              decoration:
-              BoxDecoration(color: Color(0xffF0F6F5),
-                borderRadius: BorderRadius.circular(10)
-              ),
-              child: Center(
-                child: Image.asset("assets/images/upwork.png")
-              ),
-            ),
-            trailing: Text("+ 850.0", style:
-            TextStyle(fontSize: 22,
-                fontWeight: FontWeight.bold,
-                color: Color(0xff25A969)),
-            ),
-          ),
-          ListTile(
-            title: Text("Шилжүүлэг",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-            subtitle: Text("Өчигдөр"),
-            leading: Container(
-              width: 50,
-              height: 50,
-              decoration:
-              BoxDecoration(color: Color(0xffF0F6F5),
-                  borderRadius: BorderRadius.circular(10)
-              ),
-              child: Center(
-                  child: Image.asset("assets/images/woman.png")
-              ),
-            ),
-            trailing: Text("- 85.0",
-              style: TextStyle(fontSize: 22,
-                  fontWeight: FontWeight.bold,
-              color: Color(0xffF95B51)),
-            ),
-          ),
-          ListTile(
-            title: Text("Paypal",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-            subtitle: Text("Jan 30, 2022"),
-            leading: Container(
-              width: 50,
-              height: 50,
-              decoration:
-              BoxDecoration(color: Color(0xffF0F6F5),
-                  borderRadius: BorderRadius.circular(10)
-              ),
-              child: Center(
-                  child: Image.asset("assets/images/paypal.png")
-              ),
-            ),
-            trailing: Text("+ 1406.0", style:
-            TextStyle(fontSize: 22,
-                fontWeight: FontWeight.bold,
-                color: Color(0xff25A969)),
-            ),
-          ),
-          ListTile(
-            title: Text("Youtube",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-            subtitle: Text("Jan 16, 2022"),
-            leading: Container(
-              width: 50,
-              height: 50,
-              decoration:
-              BoxDecoration(color: Color(0xffF0F6F5),
-                  borderRadius: BorderRadius.circular(10)
-              ),
-              child: Center(
-                  child: Image.asset("assets/images/youtube.png")
-              ),
-            ),
-            trailing: Text("- 11.99",
-              style: TextStyle(fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xffF95B51)),
-            ),
-          ),*/
           for (var transaction in transactions)
             ListTile(
               title: Text(transaction.title,
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
               subtitle: Text(_formatDate(transaction.date)),
               leading: Container(
                 width: 50,
                 height: 50,
                 decoration: BoxDecoration(
-                    color: Color(0xffF0F6F5),
+                    color: const Color(0xffF0F6F5),
                     borderRadius: BorderRadius.circular(10)),
                 child: Center(child: Image.asset(transaction.icon)),
               ),
@@ -419,18 +278,18 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
             ),
-          SizedBox(height: 20),
+          const SizedBox(height: 20),
           Row(
-            children: [
+            children: const [
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
+                padding: EdgeInsets.symmetric(horizontal: 20),
                 child: Text("Send Again", style:
                 TextStyle(fontSize: 18,
                     fontWeight: FontWeight.bold),),
               ),
               SizedBox(height: 15),
               Padding(
-                padding: const EdgeInsets.fromLTRB(200, 0, 0, 0),
+                padding: EdgeInsets.fromLTRB(200, 0, 0, 0),
                 child: Text("See All", style:
                 TextStyle(fontSize: 16,
                   color: Color(0xff666666),
@@ -439,10 +298,10 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ],
           ),
-          SizedBox(height: 15),
+          const SizedBox(height: 15),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
+            children: const [
               CircleAvatar(
                 backgroundImage: AssetImage('assets/images/images7.png'),
                 radius:25.0,
@@ -465,65 +324,49 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ],
           ),
-
-            ]
-
+        ]
       ),
-
+        
         bottomNavigationBar: BottomAppBar(
-          shape: CircularNotchedRectangle(),
+          shape: const CircularNotchedRectangle(),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
               IconButton(
-                icon: Icon(Icons.home_filled,
-              color: //_selectedIndex == 0
-                   Color(0xffF58742)
-                  //: Colors.grey
-                  ,
+                icon: const Icon(Icons.home_filled,
+              color: Color(0xffF58742),
                 size: 30,),
-                onPressed: () => _onItemTapped(0),
+                onPressed: (){},
               ),
               IconButton(
-                icon: Icon(Icons.local_activity_outlined,
+                icon: const Icon(Icons.local_activity_outlined,
                     size: 30),
                 onPressed: (){
                   Navigator.pushNamed(context, '/lottery');
                 },
               ),
-              SizedBox(),
+              const SizedBox(),
               IconButton(
-                icon: Icon(Icons.wallet_outlined,
+                icon: const Icon(Icons.wallet_outlined,
                     size: 30),
-                onPressed: () =>  Navigator.pushNamed(context,
-                    "/wallet"),
+                onPressed: () =>  Navigator.pushNamed(context, "/wallet"),
               ),
               IconButton(
-                icon: Icon(Icons.person
-                    ,
+                icon: const Icon(Icons.person,
                     size: 30),
-                onPressed: () {
-                  // Add your action for the circular button here
-                  // This will be triggered when the user taps on the circle button
-
-                },
+                onPressed: () {},
               ),
             ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            // Add your action for the circular button here
-            // This will be triggered when the user taps on the circle button
-            Navigator.pushNamed(context,
-                "/wallet/add");
+            Navigator.pushNamed(context, "/wallet/add");
           },
-          child: Icon(Icons.add),
-          backgroundColor: Color(0xffF58742)
-          ,
+          backgroundColor: const Color(0xffF58742),
+          child: const Icon(Icons.add),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked
-
     );
   }
 }
@@ -565,9 +408,6 @@ class TransactionData {
         break;
       case 'House Rent':
         icon = 'assets/images/house.png';
-        break;
-      case 'Netflix':
-        icon = 'assets/images/netflix.png';
         break;
       case 'Netflix':
         icon = 'assets/images/netflix.png';
