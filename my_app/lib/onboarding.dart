cc4546b2a95125c5f3ef30eeda9a5a7630cd0b8b
AriukaPG <112913238+AriukaPG@users.noreply.github.com>
2023-12-07 15:09:33 +0800

    Update onboarding.dart

--- my_app/lib/onboarding.dart
+++ my_app/lib/onboarding.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/login_screen.dart';
 
 class OnBoarding extends StatefulWidget {
   const OnBoarding({Key? key}) : super(key: key);
@@ -19,48 +18,41 @@ class _OnBoarding extends State<OnBoarding> {
               height: 450.0,
               width: 415.0,
               decoration: const BoxDecoration(
-              borderRadius: BorderRadius.vertical(bottom: Radius.elliptical(350, 50)),
-             color: Color(0xffF58742),
-              ),
+              borderRadius: BorderRadius.vertical(bottom: Radius.elliptical(350, 50)), 
+                color: Color(0xffF58742),
+              ), 
               child: Image.asset('assets/images/login.png'),
             )
           ],),
-          SizedBox(height: 50),
-          Text(
+          const SizedBox(height: 50),
+          const Text(
             'ИЛҮҮ ХУРДАН\nИЛҮҮ ХЭМНЭЛТТЭЙ',
             textAlign:  TextAlign.center,
             style: TextStyle(color:  Color(0xff464444),
-                fontSize: 24, fontWeight: FontWeight.bold)
-            ,
+                fontSize: 24, fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: 30),
-          Text(
+          const SizedBox(height: 30),
+          const Text(
             'Сугалааны ертөнцэд тавтай морил!',
             textAlign:  TextAlign.center,
-            style: TextStyle(color:  Color(0xff464444),
-                fontSize: 18)
-            ,
+            style: TextStyle(color:  Color(0xff464444), fontSize: 18),
           ),
-          SizedBox(height: 100),
+          const SizedBox(height: 100),
           Row(
-    children: [
-      SizedBox(width: 30),
-            InkWell(
-              onTap: () {
-                Navigator.pushNamed(
-                    context,"/login");
-                //MaterialPageRoute(builder: (context) => RegisterScreen()),
-                //);
-              },
-              child: Container(
-                width: 180,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Color(0xffF58742),
-                  borderRadius: BorderRadius.circular(0),
-
+            children: [
+              const SizedBox(width: 30), 
+              InkWell(
+                onTap: () {
+                  Navigator.pushNamed(context,"/login");
+                  }, 
+                child: Container(
+                  width: 180, 
+                  height: 50, 
+                  decoration: BoxDecoration(
+                    color: const Color(0xffF58742), 
+                    borderRadius: BorderRadius.circular(0),
                 ),
-                child: Center(
+                child: const Center(
                   child: Text(
                     'Sign In',
                     style: TextStyle(
@@ -69,103 +61,36 @@ class _OnBoarding extends State<OnBoarding> {
                       fontSize: 20,
                     ),
                   ),
-
                 ),
               ),
-            ),
-      InkWell(
-        onTap: () {
-          Navigator.pushNamed(
-              context,"/register");
-          //MaterialPageRoute(builder: (context) => RegisterScreen()),
-          //);
-        },
-        child: Container(
-          width: 180,
-          height: 50,
-          decoration: BoxDecoration(
-            color: Color(0xffD2D2D2),
-            borderRadius: BorderRadius.circular(0),
-
-          ),
-          child: Center(
-            child: Text(
-              'Register',
-              style: TextStyle(
-                color: Color(0xff545151),
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
+            ), 
+              InkWell(
+                onTap: () {
+                  Navigator.pushNamed(context,"/register");
+                  },
+                child: Container(
+                  width: 180, 
+                  height: 50, 
+                  decoration: BoxDecoration(
+                    color: const Color(0xffD2D2D2), 
+                    borderRadius: BorderRadius.circular(0),
+                  ), 
+                  child: const Center(
+                    child: Text(
+                      'Register', 
+                      style: TextStyle(
+                        color: Color(0xff545151), 
+                        fontWeight: FontWeight.bold, 
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-            ),
-
-          ),
-        ),
-      ),
-          ],)
+            ],
+          )
         ],
       ),
-
-      /*Center(child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-          Image.asset('assets/images/people.png',height: 400,width: 524,),
-        SizedBox(height: 30),
-        Text(
-          'УХААЛАГ ЗАРЦУУЛЖ \nИЛҮҮ ХЭМНЭЕ',
-          textAlign:  TextAlign.center,
-          style: TextStyle(color:  Color(0xff438883),
-              fontSize: 24, fontWeight: FontWeight.bold)
-            ,
-          ),
-        SizedBox(height: 45),
-        InkWell(
-        onTap: () {
-          Navigator.pushNamed(
-            context,"/register");
-            //MaterialPageRoute(builder: (context) => RegisterScreen()),
-          //);
-        },
-        child: Container(
-        width: 260,
-        height: 50,
-        decoration: BoxDecoration(
-        gradient: LinearGradient(
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-        colors:  <Color>[Color(0xff63b4ae), Color(0xff438883)],
-        ),
-        borderRadius: BorderRadius.circular(30),
-
-        ),
-          child: Center(
-            child: Text(
-              'Эхлэх',
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-
-        ),
-        ),
-        ),
-        SizedBox(height: 30),
-        Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Text('Хэрэглэгчийн Эрх бий юу?'
-            ,
-            style: TextStyle(backgroundColor: Colors.white),),
-        InkWell(
-          onTap: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) =>
-                LoginScreen()/*context, "/login"*/),);
-          },
-          child: new Text("Нэвтрэх", style: TextStyle(color: Color(0xff32819a))),
-        ),
-
-          ],
-        ),
-        SizedBox(height: 30),
-        ],*/);
+    );
   }
 }
